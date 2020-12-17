f6edeab62cf7a3483119e6f44547f1808cf2dfa0
illopa <illopa@velleitario.it>
2020-12-18 00:09:26 +0100

    some matrix work

--- 17/matrix.js
+++ 17/matrix.js
@@ -1,5 +1,4 @@
-
-var matrix = function() {
+const matrix = function() {
     return {
         // V(m,coord) return m[coord] es. V(m) return m, V(m,[]) return m V(m,[i]) return m[i], V(m,[i,j]) return m[i][j], ...
         V: function(m,coord) {
@@ -8,6 +7,7 @@ var matrix = function() {
                 return m && m[coord[0]] && this.V.call(this, m[coord[0]], coord.slice(1)) || 0;
             } else { return m; }
         },
+
         // S(m,coord,v) -> m[coord] = v es. V(m) return, V(m,[],v) return, V(m,[i],v) ->  m[i] = v, V(m,[i,j], v) -> m[i][j], = v
         S: function(m,coord,v) {
             if (m && coord && coord.length > 0) {
@@ -18,41 +18,27 @@ var matrix = function() {
                 } 
             }
         },
-        /*
-        U: function(m,n,v) {
-            let mu = [];
-            if ( n <= 0 ) { return mu; } 
-            mu[0] = v;
-            if ( n == 1 ) { mu.concat} 
-            m.reduce( (mr,mi,i) => mr.concat(this.U(mi,n-1,v), mu)
-            mu = mu.concat  this.U(m[i],n-1,v])
-            }
-        } 
-        */
+
+        // matrix dimensions
+        D: function(m) {
+            if ( !Array.isArray(m) ) { return null; }
+            let l = m.length;
+            if (!l) { return null;}
+            let c = this.D(m[0]);
+            if ( !c ) { return [l]; }
+            return [l].concat(c); 
+        },
+
+        // unshift
+        U: function(m,v) {
+            let mu = _.cloneDeep(m);
+            let c = this.D(mu);
+            let uu = [];
+            this.S(uu,c,v);
+            mu = mu.map( (mi) => this.U(mi,v) );
+            mu.unshift(uu);
+        }
 
     };
 }();
-
-
-let a = [];
-a[1] = 10;
-a[0] = [1,2,3];
-a[3] = [0,[1,2,3],3];
-console.log('A=',matrix.V(a));
-console.log('A=',matrix.V(a,[]));
-console.log('A:0,2=',matrix.V(a,[0,2]));
-console.log('A:3,1,1=',matrix.V(a,[3,1,1]));
-console.log('A:0=',matrix.V(a,[0]));
-
-console.log('5:4,3=',matrix.V(5,[4,3]));
-console.log('null',matrix.V(null));
-// console.log('undef',matrix.V(f));
-
-matrix.S(a,[3,1,1],7);
-console.log('A:3,1,1=',matrix.V(a,[3,1,1]));
-
-matrix.S(a,[10,1,1],[1,2,3]);
-console.log('a:10,1,1=',matrix.V(a,[10,1,1]));
-matrix.S(a,[9,1],5);
-console.log('A=',matrix.V(a));
-// TODO S = set, U = unshift
+(typeof module !== "undefined") && (module.exports = matix);
