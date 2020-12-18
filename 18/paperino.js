f828ad35947f405ecb4f842d7f34e6c4125a087b
illopa <illopa@velleitario.it>
2020-12-18 23:46:30 +0100

    day 18 recursive solution

--- 18/paperino.js
+++ 18/paperino.js
@@ -1,21 +1,13 @@
+var fs = require('fs');
+
 /*
 
-1 + 2 * 3 + 4 * 5 + 6
+    exp = [ [op,exp1],[op,exp2],[op,exp3],..   ]
+    exp = value
 
-1 + (2 * 3) + (4 * (5 + 6))
+ */
 
-((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
-
-*/
-
-const { isArray } = require("lodash");
-
-let line ="((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
-
-let stem = line.replace(/\(/g,'( ').replace(/\)/g,' )').split(' ',);
-
-
-function doexp(stem,exp) {
+const get_exp1 = function (stem,exp) {
     // console.log('EXP',exp);
     if (stem.length === 0) { return exp; }
     let op = stem.shift();
@@ -34,71 +26,81 @@ function doexp(stem,exp) {
             // console.log('S',sub);           
         } 
         sub.unshift('+');
-        let pe = doexp(sub,[]) 
+        let pe = get_exp1(sub,[]) 
         exp.push([op,pe]);
     } else {
         exp.push([op,c]);
     }
 
-    return doexp(stem,exp);
-}
+    return get_exp1(stem,exp);
+};
 
-function doexpmprec(stem,exp) {
+const get_exp2 = function(stem,exp) {
     // console.log('EXP',exp);
     if (stem.length === 0) { return exp; }
     let op = stem.shift();
     let c = stem.shift();
     // console.log('OP C',op,c);
-    if ( c === '(') {
-        let p = 1;
-        let e;
-        let sub = [];
-        while ( p ) {
-            e = stem.shift();
-            // console.log('E',e);
-            if ( e === ')') { p--; }
-            if ( e === '(') { p++; }
-            if ( p ) { sub.push(e); } 
-            // console.log('S',sub);           
-        } 
-        sub.unshift('+');
-        let pe = doexpmprec(sub,[]) 
-        exp.push([op,pe]);
-    } else if ( op === '*') {
+    if ( op === '*') {
         stem.unshift(c);
         stem.unshift('+');
-        let pe = doexpmprec(stem,[]) 
+        // console.log('*',stem)
+        let pe = get_exp2(stem,[]) 
         exp.push([op,pe]);
         return exp;
+    } else if ( c === '(') {
+        let p = 1;
+        let e;
+        let sub = [];
+        while ( p ) {
+            e = stem.shift();
+            // console.log('E',e);
+            if ( e === ')') { p--; }
+            if ( e === '(') { p++; }
+            if ( p ) { sub.push(e); } 
+            // console.log('S',sub);           
+        } 
+        sub.unshift('+');
+        // console.log('(',sub);
+        let pe = get_exp2(sub,[]) 
+        exp.push([op,pe]);
     } else {
         exp.push([op,c]);
     }
 
-    return doexpmprec(stem,exp);
-}
+    return get_exp2(stem,exp);
+};
 
-function docalc(exp,tot) {
+function do_calc(exp,tot) {
     if (!tot) { tot = 0; }
     if (exp.length === 0) { return tot; }
     let [op, e ] = exp.shift();
     let n = 0;
     if ( Array.isArray(e)) {
-        n = docalc(e,0);
+        n = do_calc(e,0);
     } else { n = parseInt(e); }
     if (op === '+') { tot += n;}
     if (op === '*') { tot = tot * n;}
-    return docalc(exp,tot);
+    return do_calc(exp,tot);
 }
 
-stem.unshift('+');
-console.log(stem);
+function do_hw(hw,fn) {
+    return hw.reduce( (r,line) => {
+        let stem = line.replace(/\(/g,'( ').replace(/\)/g,' )').split(' ',);
+        stem.unshift('+');
+        let exp = fn.call(this,stem,[]);    
+        let lineres = do_calc(exp,0);
+        // console.log('LC',lineres)
+        return r + lineres;
+    },0);
+}
 
-// let exp = doexp(stem,[]);
-let exp = doexpmprec(stem,[]);
+let hw = fs.readFileSync('input.txt').toString().replace(/\r/g,"").split("\n");
 
-console.log('EXP',JSON.stringify(exp,null,2));
+let res = do_hw(hw,get_exp1);
 
-let res = docalc(exp,0);
+console.log('RES PART 1',res);
 
-console.log('RES',res);
+res = do_hw(hw,get_exp2);
 
+console.log('RES PART 2',res);
