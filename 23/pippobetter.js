6cf707976a99f921b789f84390c998b330363b50
illopa <illopa>
2020-12-23 11:02:35 +0100

    day 23 part 2 better

--- 23/pippobetter.js
+++ 23/pippobetter.js
@@ -3,20 +3,18 @@ function displaynext(cup,next,i) {
     let c = cup[i];
     display.push('('+c+')');
     let n = i;
-    let r = 0;
     do {
         n = next[n];
         display.push(cup[n]);
-        r++;
-    } while ( n !== i && r < 5)
+    } while ( n !== i)
     display.pop();
     return display;
 }
 
 function movebetter(cup, max, next, pos, i, round) {
-    // console.log('-- move:',round),'--';
+    console.log('-- move:',round),'--';
     let c = cup[i];
-    // console.log('cups:',  displaynext(cup,next,i).join(' ')   );
+    console.log('cups:',  displaynext(cup,next,i).join(' ')   );
 
     // console.log('next:',next);
     let p0 = next[i];
@@ -28,7 +26,7 @@ function movebetter(cup, max, next, pos, i, round) {
     next[p2] = null;
 
     let pick = [ cup[p0], cup[p1], cup[p2] ];
-    // console.log('pick up:',pick.join(' '));
+    console.log('pick up:',pick.join(' '));
 
     let d = c-1;
     if ( d <= 0) { d = max; }
@@ -36,7 +34,7 @@ function movebetter(cup, max, next, pos, i, round) {
         d--;
         if ( d <= 0) { d = max; }
     }
-    // console.log('destination:',d);
+    console.log('destination:',d);
 
     let posd = pos[d];
     let nexd = next[posd];
@@ -45,7 +43,7 @@ function movebetter(cup, max, next, pos, i, round) {
 
     i = next[i];
 
-    // console.log('');
+    console.log('');
     return [next,i];
 }
 
@@ -55,58 +53,24 @@ let puzzle = "215694783";
 let cup = puzzle.split('').map( (n) => parseInt(n));
 console.log(cup);
 
-let max = 1000000;
+let max = 9;
 let next = [];
 let pos = [];
 cup.forEach( (c,i) => { pos[c]= i; next[i] = i+1;} );
-
-cup[max-1]=max;
-for(let n=10; n<= max; n++) {
-    cup[n-1]=n;  
-
-    pos[n]= n-1; 
-    next[n-1] = n;    
-}
-
 next[cup.length-1] = 0;
 
-/*
-pos[0] = '';
-console.log('POS',pos.slice(0,15));
-console.log('CUP',cup.slice(0,15));
-console.log('NEXT',next.slice(0,15));
-
-console.log('POS',pos.slice(max-2));
-console.log('CUP',cup.slice(max-2));
-console.log('NEXT',next.slice(max-2));
-*/
-
-console.log('STARTING');
-
-let start = new Date().getTime();
-
-let j =pos[1];
 
 let i = 0;
-let numrounds = 10000000;
+let numrounds = 100;
 for(let round= 1; round <= numrounds; round++) {
     [next,i] =  movebetter(cup, max, next, pos, i, round);
-
-    if (round % 100000 === 0 ) {
-        let step = new Date().getTime();
-        let j = cup.indexOf(1);
-        console.log('ROUND',round,'TIME',(step-start)/1000,'I',i, displaynext(cup,next,i).join(' '),'J',j, displaynext(cup,next,j).join(' '));
-    }    
 }
 
-let cupfd = displaynext(cup,next,j);
-
 console.log('-- final --')
-console.log('cups:', cupfd.join(' ')   );
+console.log('cups:',  displaynext(cup,next,i).join(' ')   );
 console.log('');
 
-let res = cupfd[1]*cupfd[2];
-
-console.log('illopa says:', res )
+let j =pos[1];
+console.log('illopa says:', displaynext(cup,next,j).slice(1).join('') )
 
 
