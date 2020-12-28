f12d14595e50772db142098e420e52a9786dac55
illopa <illopa>
2020-12-28 12:04:06 +0100

    day 23 even better

--- 23/pippoevenbetter.js
+++ 23/pippoevenbetter.js
@@ -25,10 +25,10 @@ const cupcircle = function() {
     }
 
     function movebetter(round) {
-        // console.log('-- move:',round),'--';
+        console.log('-- move:',round),'--';
         let i = current; 
         let c = val(i);
-        // console.log('cups:',  displaynext(i,9) );
+        console.log('cups:',  displaynext(i,9) );
     
         let p0 = next(i);
         let p1 = next(p0);
@@ -39,13 +39,13 @@ const cupcircle = function() {
         nextincircle[p2] = null;
     
         let pick = [ p0, p1, p2 ];
-        // console.log('pick up:',pick.join(' '));
+        console.log('pick up:',pick.join(' '));
     
         let d = prev(c);
         while ( pick.indexOf(d) >= 0 ) {
             d = prev(d);
         }
-        // console.log('destination:',d);
+        console.log('destination:',d);
     
         let nexd = next(d);
         nextincircle[d] = p0;
@@ -53,7 +53,7 @@ const cupcircle = function() {
     
         current = next(i);
     
-        // console.log('');
+        console.log('');
     }    
 
     function displaynext(i,maxd) {
@@ -72,38 +72,26 @@ const cupcircle = function() {
     }    
 
     return {
-        init_part2: function(puzzle,maxcircle,numrounds){
+        init_part1: function(puzzle,maxcircle,numrounds){
             num = numrounds;
             max = maxcircle;
             cup  = puzzle.split('').map( (n) => parseInt(n));
             current = cup[0];
 
-            cup.forEach( (v,i) => { nextincircle[v] = cup[i+1] || i+2;} );
+            cup.forEach( (v,i) => { nextincircle[v] = cup[i+1] || current;} );
             console.log(cup);
             console.log(nextincircle);
-            nextincircle[max] = current;
-
         },
         do: function(maxdisplay) {
             console.log('STARTING');
 
-            let start = new Date().getTime();
-
-            let j  = 1;
-          
             for(let round= 1; round <= num; round++) {
-                movebetter(round);
-            
-                if (round % 100000 === 0 ) {
-                    let step = new Date().getTime();
-                    console.log('ROUND',round,'TIME',(step-start)/1000,'CURR',current, displaynext(current,maxdisplay),'J',j, displaynext(j,maxdisplay));
-                }    
+                movebetter(round);   
             }
             
-        
         },
 
-        part2: function(maxdisplay) {
+        part1: function(maxdisplay) {
             let j  = 1;
 
             let cupfd = displaynext(j,maxdisplay);
@@ -112,10 +100,11 @@ const cupcircle = function() {
             console.log('cups:', cupfd  );
             console.log('');
             
-            let cupfda = cupfd.split(' ');
-            let res = cupfda[1]*cupfda[2];
-            
+            let res = cupfd.split(' ').slice(1).join('');
+ 
             console.log('illopa says:', res )    
+
+    
         }
     }
 }();
@@ -123,11 +112,11 @@ const cupcircle = function() {
 
 // let puzzle = "389125467";
 let puzzle = "215694783";
-let max = 1000000;
-let numrounds = 10000000;
+let max = 9;
+let numrounds = 100;
 
-cupcircle.init_part2(puzzle,max,numrounds);
+cupcircle.init_part1(puzzle,max,numrounds);
 
-cupcircle.do(3);
+cupcircle.do(9);
 
-cupcircle.part2(3);
+cupcircle.part1(9);
